8cc8c1d98b2730181baeb86c6cd79803535e0878
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-04 21:47:38 +0900

    Added and solved Task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,11 @@
+﻿//повернуть слово
+string word = "привет";
+char[] newWord = new char[word.Length];
+
+
+for (int i = 0; i < word.Length; i++)
+{
+    newWord[i]= word[word.Length-1-i];    
+}
+
+System.Console.WriteLine(newWord);
