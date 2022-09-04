cb0d98041db02d86a2b6c671e366b2cba719f141
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-04 21:05:34 +0900

    Task 4 added and solved

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿nt Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void Square(int number)
+{
+    int index = 1;
+    while (index <= number)
+    {
+        System.Console.WriteLine($"Квадрат числа {index} это {index * index}");
+        index++;
+    }
+}
+int number = Prompt("Введите число:");
+Square(number);
